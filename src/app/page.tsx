f62a98fbf0d50f209b09607e743269b682b61085
tochisuke221 <tochisuke221@gmail.com>
2023-10-07 16:35:51 +0900

    環境構築

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useForm, SubmitHandler } from "react-hook-form"
+import ContentLoader from 'react-content-loader'
 
 type myFormData = {
   firstName: string;
@@ -8,26 +9,32 @@ type myFormData = {
   category: string;
 }
 
-
 export default function Home() {
-  const { register, handleSubmit, formState: { errors } } = useForm<myFormData>()
+  const { register, handleSubmit, formState: { errors } } = useForm<myFormData>();
+
   const onSubmit: SubmitHandler<myFormData> = (data) => {
     console.log(data)
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName', { required: true })} placeholder="名前" />
-      {errors.firstName && <div>名前を入力してください</div>}
-      <input {...register('lastName', { required: true })} placeholder="名字" />
-      {errors.lastName && <div>名字を入力してください</div>}
-      <select {...register('category', { required: true })}>
-        <option value="">選択...</option>
-        <option value="A">カテゴリーA</option>
-        <option value="B">カテゴリーB</option>
-      </select>
-      {errors.category && <div>カテゴリーを選択してください</div>}
-      <input type="submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName', { required: true })} placeholder="名前" />
+        {errors.firstName && <div>名前を入力してください</div>}
+        <input {...register('lastName', { required: true })} placeholder="名字" />
+        {errors.lastName && <div>名字を入力してください</div>}
+        <select {...register('category', { required: true })}>
+          <option value="">選択...</option>
+          <option value="A">カテゴリーA</option>
+          <option value="B">カテゴリーB</option>
+        </select>
+        {errors.category && <div>カテゴリーを選択してください</div>}
+        <input type="submit" />
+      </form>
+      <ContentLoader>
+        <rect x="0" y="0" rx="5" ry="5" width="70" height="70"></rect>
+        {/* 他のローディングプレースホルダーの要素を追加 */}
+      </ContentLoader>
+    </div>
   )
 }
