21771faf16d83f43ac73f5c1309eae885dd80152
tochisuke221 <tochisuke221@gmail.com>
2023-10-07 16:07:21 +0900

    React Hook Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type myFormData = {
+  firstName: string;
+  lastName: string;
+  category: string;
+}
+
 
 export default function Home() {
+  const { register, handleSubmit, formState: { errors } } = useForm<myFormData>()
+  const onSubmit: SubmitHandler<myFormData> = (data) => {
+    console.log(data)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('firstName', { required: true })} placeholder="名前" />
+      {errors.firstName && <div>名前を入力してください</div>}
+      <input {...register('lastName', { required: true })} placeholder="名字" />
+      {errors.lastName && <div>名字を入力してください</div>}
+      <select {...register('category', { required: true })}>
+        <option value="">選択...</option>
+        <option value="A">カテゴリーA</option>
+        <option value="B">カテゴリーB</option>
+      </select>
+      {errors.category && <div>カテゴリーを選択してください</div>}
+      <input type="submit" />
+    </form>
   )
 }
